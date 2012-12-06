70f3dcb6d855a4c0f0314bfad2fa6942a92a4df0
Daniel Lucraft <dan@fluentradical.com>
2012-12-06 12:16:51 +0000

    Move aspec binary here from caltrka

--- lib/aspec.rb
+++ lib/aspec.rb
@@ -1,5 +1,41 @@
 
 
+class Aspec
+  def self.configuration
+    @configuration ||= Configure.new
+  end
 
-module Aspec
+  def self.configure
+    yield configuration
+  end
+
+  class Configure
+    def initialize
+    end
+
+    def app_under_test(&block)
+      @app_under_test = block
+    end
+
+    def before(&block)
+      @before = block
+    end
+
+    def get_app_under_test
+      @app_under_test
+    end
+
+    def get_before
+      @before
+    end
+
+    def after_suite(&block)
+      @after_suite = block
+    end
+
+    def get_after_suite
+      @after_suite
+    end
+  end
 end
+

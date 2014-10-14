fd9bed2abb47c76f155b933ffba0c7587d4cc966
Paul Springett <paul.springett@songkick.com>
2014-10-14 15:46:27 +0100

    Use RSpec expect syntax in specs

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -7,8 +7,10 @@ describe Aspec::CLI do
 
   it "should list all files in a directory" do
     cli = Aspec::CLI.new(test_app_dir, ["aspec/"])
-    cli.aspec_files.sort.should == [
-      File.expand_path("aspec/failing.aspec", test_app_dir), 
+    expected_results = [
+      File.expand_path("aspec/failing.aspec", test_app_dir),
       File.expand_path("aspec/passing.aspec", test_app_dir)].sort
+
+    expect(cli.aspec_files.sort).to eq expected_results
   end
 end
